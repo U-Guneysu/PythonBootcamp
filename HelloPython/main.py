a94a7f29f76f1a4953f0e1ac3ace087c9bd07635
UmutG. <umutguneysuu@gmail.com>
2025-10-16 19:07:58 +0300

    Bölüm 14 - PackageAndModule

---
+++ HelloPython/main.py
@@ -0,0 +1,12 @@
+#import umutmodule
+from random import randint
+from umutmodule import example_func
+from AnimalPackage import info
+from AnimalPackage.CatPackage import meow
+
+example_func()
+print("hello world")
+
+info.info()
+
+meow.test()
